81279ae3b4ad74e34ee21dda9c4e0b210c3715e4
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-12-15 18:32:27 +0100

    Inheritance???

---
+++ Inheritance/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+   public class Stack
+    {
+
+        // Properties
+        private List<object> stackList { get; set; }
+
+        public Stack()
+        {
+            stackList = new List<object> { };
+        }
+
+        // Methods
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Null value passed as a parameter");
+            }
+            else
+            {
+                stackList.Add(obj);
+            }
+            
+        }
+        public object Pop()
+        {
+            var upperElement = stackList.Last();
+
+            var index = (stackList.Count()) - 1;
+            stackList.RemoveAt(index);
+            return upperElement;
+        }
+
+        public void Clear()
+        {
+            stackList.Clear();
+        }
+    }
+}
