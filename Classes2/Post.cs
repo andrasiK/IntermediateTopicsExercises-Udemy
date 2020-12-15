1e294d4e2f1546c63db65c9a5efdfd1fc5a83544
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-12-15 12:10:54 +0100

    classes2

---
+++ Classes2/Post.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2
+{
+    public class Post
+    {
+        //Properties: Title, Description, DateTime when was created
+
+        // change in votes: from outside not should be able to modiify vote -
+        //create a readonly field, which will be assigned here in class from VOte
+        public string Title { get; set; }
+        public string  Description { get; set; }
+        private DateTime Creation { get; set; }
+        public int Votes { get; set; }
+        //Methods: Vote up, Vote down, see  current vote value
+
+
+        public Post()
+        {
+            Votes = 0;
+            Creation = DateTime.Now;
+        }
+
+
+        public void VoteUp()
+        {
+            Votes++;
+        }
+
+       public void VoteDown()
+        {
+            Votes--;
+        }
+
+    }
+}
